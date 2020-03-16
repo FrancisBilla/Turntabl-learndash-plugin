1174fe9c7507b7384bf8ef50200fd13122936cec
FrancisBilla <francis.billa.fb@gmail.com>
2020-03-16 10:05:48 +0000

    new feature

---
+++ turntabl-lashdash-plugin.php
@@ -0,0 +1,32 @@
+<?php
+/**
+* @package Turntabl-LearnDash-Plugin
+*/
+/*
+Plugin Name: Turntabl-LearnDash Plugin
+Plugin URI: https://github.com/francisbilla/turntabl-learnDash-Plugin
+Description: This is a Plugin design by Turntabl Ghana to provide tool to build an affordable Learning Management System to our Clients.
+Version: 1.0.0
+Author: Francis Billa *PROPHET* & Samuel Owusu *Sam K*
+Author URI: https://github.com/francisbilla
+License: GPLv2 or later
+Text Domain:  Turntabl-learndash-plugin
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2005-2015 Automatic, Inc.
+*/
